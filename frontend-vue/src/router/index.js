099c6676665cc3c04e869d0d5d24aee323bdddc5
DWL5 <ssop6403@gmail.com>
2022-10-05 22:59:26 +0900

    feat: 레이아웃 분할

---
+++ frontend-vue/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from  'vue-router'
+import PageHome from "@/views/PageHome";
+
+const routes = [
+  {
+    path: '/',
+    name: 'PageHome',
+    component: PageHome
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import('../views/PageAbout')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
